e4f7616c00faad66dcd0629256ee3d188d46383a
ericcj24 <dannycj11@gmail.com>
2017-12-02 13:39:09 +0000

    second commit, seam finding

--- SeamCarver.java
+++ SeamCarver.java
@@ -8,27 +8,32 @@ public class SeamCarver {
 	private Picture pic;
 	private double[][] cachedEnergy;
 	private boolean transposed = false;
+	private boolean isColorMatrixTransposed = false;
 	private Color[][] colors;
 
 	// create a seam carver object based on the given picture
-   public SeamCarver(Picture picture) {
-	   if (null == picture) {
-		   throw new java.lang.IllegalArgumentException();
+	public SeamCarver(Picture picture) {
+		if (null == picture) {
+			throw new java.lang.IllegalArgumentException();
+		}
+
+		// defensive copy
+		this.pic = new Picture(picture);
+		this.colors = new Color[pic.height()][pic.width()];
+		this.cachedEnergy = new double[pic.height()][pic.width()];
+		initializeCachedEnergy();
+	}
+
+	private void initializeCachedEnergy() {
+	   for (int c=0; c<pic.width(); c++) {
+		   for (int r=0; r<pic.height(); r++) {
+			   colors[r][c] = pic.get(c, r);
+		   }
 	   }
 
-	   // defensive copy
-	   this.pic = new Picture(picture);
-	   this.colors = new Color[pic.width()][pic.height()];
-
-	   this.cachedEnergy = new double[pic.width()][pic.height()];
-	   calculateEnergy();
-   }
-
-	private void calculateEnergy() {
-	   for (int i=0; i<pic.width(); i++) {
-		   for (int j=0;j <pic.height(); j++) {
-			   colors[i][j] = pic.get(i, j);
-			   cachedEnergy[i][j] = energy(i,j);
+	   for (int c=0; c<pic.width(); c++) {
+		   for (int r=0; r<pic.height(); r++) {
+			   cachedEnergy[r][c] = energy(c,r);
 		   }
 	   }
 	}
@@ -40,29 +45,46 @@ public class SeamCarver {
 
    // width of current picture
    public int width() {
-	   return this.pic.width();
+	   int width = this.colors[0].length;
+	   int height = this.colors.length;
+	   if (isColorMatrixTransposed) {
+		   width = height;
+	   }
+	   return width;
    }
 
    // height of current picture
    public int height() {
-	   return this.pic.height();
+	   int width = this.colors[0].length;
+	   int height = this.colors.length;
+	   if (isColorMatrixTransposed) {
+		   height = width;
+	   }
+	   return height;
    }
 
    // energy of pixel at column x and row y
    public double energy(int x, int y) {
-	   if (x==0 || x==pic.width()-1 || y==0 || y==pic.height()-1) {
+	   int colorMatrixX = y;
+	   int colorMatrixY = x;
+	   if (isColorMatrixTransposed) {
+		   colorMatrixX = x;
+		   colorMatrixY = colors[0].length - 1 - y;
+	   }
+
+	   if (colorMatrixX==0 || colorMatrixX==colors.length-1 || colorMatrixY==0 || colorMatrixY==colors[0].length-1) {
 		   return 1000;
 	   }
 
-	   double energyXSquared = calcEnergy(x-1, y, x+1, y);
-	   double energyYSquared = calcEnergy(x, y-1, x, y+1);
+	   double energyXSquared = calcEnergy(colorMatrixX-1, colorMatrixY, colorMatrixX+1, colorMatrixY);
+	   double energyYSquared = calcEnergy(colorMatrixX, colorMatrixY-1, colorMatrixX, colorMatrixY+1);
 	   return Math.sqrt(energyXSquared+energyYSquared);
    }
 
    private double calcEnergy(int x1, int y1, int x2, int y2) {
-	   double deltaR = pic.get(x1, y1).getRed() - pic.get(x2, y2).getRed();
-	   double deltaG = pic.get(x1, y1).getGreen() - pic.get(x2, y2).getGreen();
-	   double deltaB = pic.get(x1, y1).getBlue() - pic.get(x2, y2).getBlue();
+	   double deltaR = colors[x1][y1].getRed() - colors[x2][y2].getRed();
+	   double deltaG = colors[x1][y1].getGreen() - colors[x2][y2].getGreen();
+	   double deltaB = colors[x1][y1].getBlue() - colors[x2][y2].getBlue();
 
 	   double deltaSquare = deltaR*deltaR + deltaG*deltaG + deltaB*deltaB;
 	   return deltaSquare;
@@ -74,32 +96,63 @@ public class SeamCarver {
 	   if (!transposed) {
 		   transpose();
 	   }
-	   int[] shortestPath = findVerticalSeam();
 
+	   int[] shortestPath = new int[cachedEnergy.length];
+	   findShortestPath(shortestPath);
+
+	   for (int i=0; i<shortestPath.length; i++) {
+		   shortestPath[i] = cachedEnergy[0].length - shortestPath[i] -1;
+	   }
 	   return shortestPath;
    }
 
 
 	// sequence of indices for vertical seam
 	public int[] findVerticalSeam() {
-
 		if (transposed) {
 		   transpose();
 		}
 
-		int[] shortestPath = new int[pic.height()];
+		int[] shortestPath = new int[cachedEnergy.length];
 		findShortestPath(shortestPath);
 		return shortestPath;
 	}
 
 	private void transpose() {
-		// TODO Auto-generated method stub
+		if (!transposed) {
+			int origWidth = cachedEnergy[0].length;
+			int origHeight = cachedEnergy.length;
 
+			double[][] tempEnergy = new double[origWidth][origHeight];
+			for (int i=0; i<origHeight; i++) {
+				for (int j=0; j<origWidth; j++) {
+					tempEnergy[j][origHeight-i-1] = cachedEnergy[i][j];
+				}
+			}
+
+			this.cachedEnergy = tempEnergy;
+		}
+		// transpose back
+		else {
+			int origWidth = cachedEnergy[0].length;
+			int origHeight = cachedEnergy.length;
+
+			double[][] tempEnergy = new double[origWidth][origHeight];
+			for (int i=0; i<origHeight; i++) {
+				for (int j=0; j<origWidth; j++) {
+					tempEnergy[origWidth-1-j][i] = cachedEnergy[i][j];
+				}
+			}
+
+			this.cachedEnergy = tempEnergy;
+		}
+
+		this.transposed = !transposed;
 	}
 
 	private void findShortestPath(int[] shortestPath) {
-	   int width = pic.width();
-	   int height = pic.height();
+	   int width = cachedEnergy[0].length;
+	   int height = cachedEnergy.length;
 	   int length = width*height;
 	   double[] distTo = new double[length+1];
 	   for (int i=0; i<length+1; i++) {
@@ -108,7 +161,7 @@ public class SeamCarver {
 
 	   int[] edgeTo = new int[length+1];
 	   for (int c = 0; c <width; c++) {
-		   dfs(c, 0, distTo, edgeTo, cachedEnergy[c][0], 0);
+		   dfs(c, 0, distTo, edgeTo, cachedEnergy[0][c], 0);
 	   }
 
 	   int runner = edgeTo.length-1;
@@ -122,25 +175,25 @@ public class SeamCarver {
    private void dfs(int c, int r, double[] distTo, int[] edgeTo, double prevEnergy, int prevC) {
 	   relax(c, r, distTo, edgeTo, prevEnergy, prevC);
 	   // reached the bottom row, terminate
-	   if (r==pic.height()) {
+	   if (r==cachedEnergy.length) {
 		   return;
 	   }
 
 	   if (c-1>=0) {
-		   dfs(c-1, r+1, distTo, edgeTo, cachedEnergy[c][r], c);
+		   dfs(c-1, r+1, distTo, edgeTo, cachedEnergy[r][c], c);
 	   }
-	   dfs(c, r+1, distTo, edgeTo, cachedEnergy[c][r], c);
-	   if (c+1<=pic.width()-1) {
-		   dfs(c+1, r+1, distTo, edgeTo, cachedEnergy[c][r], c);
+	   dfs(c, r+1, distTo, edgeTo, cachedEnergy[r][c], c);
+	   if (c+1<=cachedEnergy[0].length-1) {
+		   dfs(c+1, r+1, distTo, edgeTo, cachedEnergy[r][c], c);
 	   }
    }
 
    private void relax(int c, int r, double[] distTo, int[] edgeTo, double prevEnergy, int prevC) {
-	   int width = pic.width();
+	   int width = cachedEnergy[0].length;
 	   if (r==0) {
 		   distTo[c] = prevEnergy;
 		   edgeTo[c] = c;
-	   } else if (r==pic.height()) {
+	   } else if (r==cachedEnergy.length) {
 		   // go to last vertex
 		   int idx = r*width;
 		   int prevIdx = prevC + (r-1)*width;
@@ -163,27 +216,29 @@ public class SeamCarver {
 	   if (null == seam) {
 		   throw new java.lang.IllegalArgumentException();
 	   }
-	   if (seam.length != pic.width()) {
+	   if (seam.length != colors[0].length) {
 		   throw new java.lang.IllegalArgumentException();
 	   }
-	   if (pic.width() <= 1) {
+	   if (colors.length <= 1) {
 		   throw new java.lang.IllegalArgumentException();
 	   }
 
-	   int cache=seam[0];
-	   int height = pic.height();
-	   for (int i =1; i<seam.length ;i++) {
-		   // line is broken
-		   if (seam[i]-cache > 1) {
-			   throw new java.lang.IllegalArgumentException();
-		   }
-		   // outside range
-		   if (seam[i] >= height) {
-			   throw new java.lang.IllegalArgumentException();
-		   }
-		   cache = seam[i];
 
+	   if (!isColorMatrixTransposed) {
+		   transposeColorMatrix();
 	   }
+	   if (!transposed) {
+		   transpose();
+	   }
+
+	   int width = colors[0].length;
+	   // transfer this coordinates
+	   for (int i=0; i<seam.length; i++) {
+		   seam[i] =width - seam[i]-1;
+	   }
+
+	   colors = copyAndRemove(seam);
+
    }
 
    // remove vertical seam from current picture
@@ -191,31 +246,83 @@ public class SeamCarver {
 	   if (null == seam) {
 		   throw new java.lang.IllegalArgumentException();
 	   }
-	   if (seam.length != pic.height()) {
+	   if (seam.length != colors.length) {
 		   throw new java.lang.IllegalArgumentException();
 	   }
-	   if (pic.height() <= 1) {
+	   if (colors[0].length <= 1) {
 		   throw new java.lang.IllegalArgumentException();
 	   }
 
+	   if (isColorMatrixTransposed) {
+		   transposeColorMatrix();
+	   }
+	   if (transposed) {
+		   transpose();
+	   }
+
+	   colors = copyAndRemove(seam);
+	   cachedEnergy = recalculateCachedEnergy(seam);
+   }
+
+   	private double[][] recalculateCachedEnergy(int[] seam) {
+
+   		return null;
+   	}
+
+   	private void transposeColorMatrix() {
+
+	   if (!isColorMatrixTransposed) {
+		   int origWidth = colors[0].length;
+		   int origHeight = colors.length;
+
+		   Color[][] temp = new Color[origWidth][origHeight];
+		   for (int i=0; i<origHeight; i++) {
+			   for (int j=0; j<origWidth; j++) {
+				   temp[j][origHeight-i-1] = colors[i][j];
+			   }
+		   }
+
+		   this.colors = temp;
+	   } else {
+		   int origWidth = colors[0].length;
+		   int origHeight = colors.length;
+
+		   Color[][] temp = new Color[origWidth][origHeight];
+		   for (int i=0; i<origHeight; i++) {
+			   for (int j=0; j<origWidth; j++) {
+				   temp[origWidth-1-j][i] = colors[i][j];
+			   }
+		   }
+
+		   this.colors = temp;
+	   }
+
+	   this.isColorMatrixTransposed = !isColorMatrixTransposed;
+   }
+
+   private Color[][] copyAndRemove(int[] seam) {
 	   int cache=seam[0];
-	   int width = pic.width();
-	   Color[][] temp = new Color[pic.width()-1][pic.height()];
+	   int width = colors[0].length;
+	   int height = colors.length;
+	   Color[][] temp = new Color[width-1][height];
 	   for (int i =1; i<seam.length ;i++) {
 		   // line is broken
 		   if (seam[i]-cache > 1) {
 			   throw new java.lang.IllegalArgumentException();
 		   }
-		   // outside range
-		   if (seam[i] >= width || seam[i]<0) {
-			   throw new java.lang.IllegalArgumentException();
-		   }
-		   cache = seam[i];
+			// outside range
+			if (seam[i] >= width || seam[i]<0) {
+				throw new java.lang.IllegalArgumentException();
+			}
+			cache = seam[i];
 
-
-		   //System.arraycopy(colors[i], srcPos, dest, destPos, length);
-
-	   }
-
-   }
+			if (seam[i] != 0) {
+				System.arraycopy(colors[i], 0, temp, seam[i]-1, seam[i]);
+				System.arraycopy(colors[i], seam[i]+1, temp, seam[i], width-seam[i]-1);
+			} else {
+				System.arraycopy(colors[i], 1, temp, 0, width-1);
+			}
+		}
+		return temp;
+	}
 }
